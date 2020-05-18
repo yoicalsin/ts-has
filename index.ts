ef0078d81d451924f304052ba06df084948f8293
Yoni Calsin <yonicb20@gmail.com>
2020-05-18 16:37:28 -0500

    style: 💄 Removed customized types !

--- index.ts
+++ index.ts
@@ -1,5 +1,21 @@
 import bind from 'function-bind';
 
-const has = bind.call(Function.call, Object.prototype.hasOwnProperty);
+interface Has {
+    (
+        object: {
+            [key: string]: any;
+        },
+        path: string | number,
+    ): boolean;
+}
+
+/**
+ * @author Yoni Calsin <helloyonicb@gmail.com>
+ * @description Object.prototype.hasOwnProperty.call shortcut for typescript, ts-has
+ * @example
+ * has({}, 'hasOwnProperty'); // false
+ * has(Object.prototype, 'hasOwnProperty'); // true
+ */
+const has: Has = bind.call(Function.call, Object.prototype.hasOwnProperty);
 
 export default has;
